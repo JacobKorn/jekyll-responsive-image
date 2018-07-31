a7ff06cc75eeadf6d6ad326eaa47d7f2cb0b6e4d
Anthony <anthony@anthonymace.com>
2018-07-31 23:51:29 +1200

    tests passing

--- lib/jekyll-responsive-image/tag.rb
+++ lib/jekyll-responsive-image/tag.rb
@@ -1,20 +1,44 @@
 module Jekyll
   module ResponsiveImage
     class Tag < Liquid::Tag
+
+      attr_reader :markup
+
       def initialize(tag_name, markup, tokens)
+        @markup = markup
         super
 
         @attributes = {}
-
-        markup.scan(::Liquid::TagAttributes) do |key, value|
-          # Strip quotes from around attribute values
-          @attributes[key] = value.gsub(/^['"]|['"]$/, '')
-        end
       end
 
       def render(context)
+
+        markup.scan(::Liquid::TagAttributes) do |key, value|
+          # Strip quotes from around attribute values
+          stripped = value.gsub(/^['"]|['"]$/, '')
+
+          if context[value]
+            @attributes[key] = context[value]
+          else
+            @attributes[key] = stripped
+          end
+
+          @attributes[key] = process_widths(stripped) if key == "widths" && stripped
+        end
+
         Renderer.new(context.registers[:site], @attributes).render_responsive_image
       end
+
+      private
+
+      def process_widths(widths)
+        widths.split(",").map do |width|
+          {
+            "width" => width.to_i
+          }
+        end
+      end
+
     end
   end
 end
